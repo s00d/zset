c5a26f83e0f8b73dd37b7048ef6a4631375e9f2a
Pavel <virus191288@gmail.com>
2018-07-13 11:35:27 +0500

    fix test

--- bench.js
+++ bench.js
@@ -1,26 +1,25 @@
-'use strict';
 let ZSet = require('./index');
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let test = new ZSet({t1: {val: 1}, t2: {val: 2}, t3: {ru: {vk: 1}}});
+let test = new ZSet({t1: {val: 1}, t2: {val: 2}, t3: {ru: {s1: 1}}});
 
 suite('ZSet_big', function () {
     let init = 0;
     for(let i=0;i<10000;i++) {
-        test.set('t'+init, {ru: {vk: getRandomInt(0, 10), inst: 10, ok: 15}, ch:  {vk: getRandomInt(0, 10)}})
+        test.set('t'+init, {ru: {s1: getRandomInt(0, 10), s2: 10, s3: 15}, ch:  {s1: getRandomInt(0, 10)}})
     }
 
     bench('full', function () {
-        test.set('t', {ru: {vk: getRandomInt(0, 10), inst: 10, ok: 15}, ch:  {vk: getRandomInt(0, 10)}});
+        test.set('t', {ru: {s1: getRandomInt(0, 10), s2: 10, s3: 15}, ch:  {s1: getRandomInt(0, 10)}});
         test.incr('t', 2);
         test.set('t1', {val: 1});
         test.get()
     });
     bench('set', function () {
-        test.set('t', {ru: {vk: getRandomInt(0, 10), inst: 10, ok: 15}, ch:  {vk: getRandomInt(0, 10)}});
+        test.set('t', {ru: {s1: getRandomInt(0, 10), s2: 10, s3: 15}, ch:  {s1: getRandomInt(0, 10)}});
     });
     bench('incr', function () {
         test.incr('t', 2);
@@ -32,6 +31,6 @@ suite('ZSet_big', function () {
         test.set('t1', {val: 1});
     });
     bench('search', function () {
-        test.search('ru.vk', (count) => count > 0);
+        test.search('ru.s1', (count) => count > 0);
     });
 });
--- test.js
+++ test.js
@@ -1,33 +1,6 @@
 import test from 'ava';
 import ZSet from './index';
 
-// function getRandomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-
-// let init = 0;
-// let test = new ZSet({t1: {val: 1}, t2: {val: 2}, t3: {ru: {vk: 1}}});
-
-// setInterval(() => {
-//     test.set('t'+init, {ru: {vk: getRandomInt(0, 10), inst: 10, ok: 15}, ch:  {vk: getRandomInt(0, 10)}})
-//     init++
-// }, 500)
-
-// setInterval(() => {
-//     let {country, item} = test.searchService('vk', (count) => count > 0);
-//     console.log(country, item);
-// }, 500)
-
-// test.incr('t3', 2)
-// test.incr('t2', 3)
-// test.incr('t1', 2)
-
-// test.set('t1', {val: 1})
-// test.set('t4', {val: 4})
-
-// console.log(test.get());
-
-
 test(t => {
     let data = {t1: {val: {val: 1}}, t2: {val: 2}, t3: {val: {val: 3}}};
     let zset = new ZSet(data);
@@ -38,9 +11,9 @@ test(t => {
     t.is(getData[Object.keys(getData)[0]].score, 1);
 
     t.is(zset.search('val.val', (count) => count > 0).key, 't1');
-    zset.incr('t1')
+    zset.incr('t1');
     t.is(zset.search('val.val', (count) => count > 0).key, 't3');
-    zset.incr('t3')
+    zset.incr('t3');
     t.is(zset.search('val.val', (count) => count > 0).key, 't1');
     
     t.is(zset.get(1,1).length, 1);
@@ -49,12 +22,12 @@ test(t => {
 
     t.is(zset.search('val.val', (count) => count > 0).score, 2);
     t.is(zset.search('val.val', (count) => count > 0).key, 't1');
-    zset.del('t1')
+    zset.del('t1');
     t.is(zset.search('val.val', (count) => count > 0).key, 't3');
-    let item = zset.search('val.val', (count) => count > 0)
+    let item = zset.search('val.val', (count) => count > 0);
     item.data.val.val -= 3;
     t.is(zset.search('val.val', (count) => count > 0), false);
-    zset.set('t4', {val: {val: 3}})
+    zset.set('t4', {val: {val: 3}});
     t.is(zset.search('val.val', (count) => count > 0).key, 't4');
     t.is(zset.byKey('t2').key, 't2');
     t.is(zset.first().key, 't2');
@@ -63,28 +36,4 @@ test(t => {
 
     zset.clear();
     t.is(zset.first(), false);
-
-
-    // let resp = network.check('vk auth key 9999')
-    // t.is(resp.code, 'vk');
-
-    // resp = network.check('vk auth 9999');
-    // t.is(resp.code, 'vk');
-
-    // resp = network.check('Кoд InstАgram поDтверждения: 109 236. #ig');
-    // t.is(resp.code, 'instagram');
-
-    // resp = network.check('12345 asdfgrrr');
-    // t.is(resp.code, false);
-
-    // resp = network.check('vk autha 12334');
-    // t.is(resp.code, 'vk');
-
-    // network.lern('123456', 'code1');
-    // network.lern('234567', 'code2');
-    // network.lern('890123', 'code3');
-    // resp = network.check('123340');
-    // logger.info('check 123340', `result: ${resp.code}`, `path: ${resp.path}`);
-
-    // fs.writeFileSync(__dirname + "/tree2.html", network.toHtml(), 'utf-8');
 });
